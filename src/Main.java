4c0b292e84e38358dd09b7b02bd0b0362a02b11d
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-29 10:31:50 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,9 @@
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Student student01 = StudentList.getStudentById(23);
-        System.out.println("student01 = " + student01);
-
-        Optional<Student> student02 = StudentList.getStudentByIdV3(45);
-        System.out.println("student02 = " + student02);
-
-        try {
-            Student student03 = StudentList.getStudentByIdV2(32);
-        } catch (Exception e) {
-            System.out.println("Student Not Found");
-        }
-
-        Student student04 = StudentList.getStudentByIdV1(55);
-        System.out.println("student04 = " + student04);
 
     }
 }
